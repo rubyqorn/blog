6566dfafd3fae70ae172b55bf223834f210ef3ea
Anton Hideger <antonhideger1337@gmail.com>
2021-01-04 23:14:04 +0300

    Implemented API method which returns posts list

---
+++ src/Controllers/API/PostsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controllers\API;
+
+use App\Models\Post;
+use App\Controllers\Controller;
+
+class PostsController extends Controller
+{
+    /**
+     * Initiates PostsController constructor method
+     * 
+     * @return void 
+     */ 
+    public function __construct()
+    {
+        parent::__construct();
+        $this->postModel = new Post();
+    }
+
+    /**
+     * Returns all posts from data with 
+     * all fields. Posts have to be returned in JSON
+     * format and if table is empty will be returned
+     * error message
+     * 
+     * @return void 
+     */ 
+    public function all()
+    {
+        $posts = $this->postModel->getPosts();
+
+        if (empty($posts)) {
+            return $this->render('api/posts');
+        }
+
+        return $this->render('api/posts', compact('posts'));
+    }
+}
